99dd3f45c81a03c1407308793f844d1f606261d2
Kareem Farrell <kareemfarrellonline@gmail.com>
2025-02-04 09:30:34 -0400

    Created environment to test properly

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.app_context().push()
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'  # return 'Hello World' in response
+
+app.run(host='0.0.0.0', port=8080, debug=True)
